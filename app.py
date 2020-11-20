0e632e8998285e4357a23c3d08b84b794d2d11f3
I518712 <macvin.pinto@sap.com>
2020-11-20 19:07:55 +0530

    Added Schedule

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from user import user_blueprint
 from medicine import medicine_blueprint
 from doctor import doctor_blueprint
 from appointment import appointment_blueprint
+from schedule import schedule_blueprint
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.register_blueprint(user_blueprint)
 app.register_blueprint(medicine_blueprint)
 app.register_blueprint(doctor_blueprint)
 app.register_blueprint(appointment_blueprint)
+app.register_blueprint(schedule_blueprint)
 
 
 if __name__ == '__main__':
